36d5843be06820d6a99567f0005e550e5c74c718
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-27 07:42:32 +0700

    refac: setup chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import type { AppProps } from "next/app";
-
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider resetCSS={true} theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
